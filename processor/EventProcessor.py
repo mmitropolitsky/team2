d9ba1aa748d94f66c8908a820a531bc5e232eb98
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2018-03-28 21:17:10 +0300

    analyzing obd

--- processor/EventProcessor.py
+++ processor/EventProcessor.py
@@ -1,15 +1,17 @@
 import time
 
+import sqlite3
+
 from alarm.Alarm import Alarm
 
-from analyzer.OdbParamAnalyzer import OdbParamAnalyzer
-from analyzer.OdbSpeedAnalyzer import OdbSpeedAnalyzer
 from analyzer.PostureAnalyzer import PostureAnalyzer
-from analyzer.OdbParamsAnalyzer import OdbParamsAnalyzer 
+from analyzer.ObdParamsAnalyzer import ObdParamsAnalyzer
+
 
 
 class EventProcessor(object):
-    __processors = [PostureAnalyzer(), OdbSpeedAnalyzer(), OdbParamAnalyzer()]
+    #__processors = [PostureAnalyzer(), ObdParamsAnalyzer()]
+    __processors = [ObdParamsAnalyzer()]
 
     __alarm = Alarm()
 
@@ -23,7 +25,8 @@ class EventProcessor(object):
         print self.__dict
 
     def process(self):
-        while True:
+        i = 0
+        while i<15:
             print 'invoking analyzers'
             should_alarm = self.__should_alarm()
             print should_alarm
@@ -31,16 +34,19 @@ class EventProcessor(object):
                 print 'alarming'
                 self.__alarm.ring()
             time.sleep(5)
+            i+=1
+        self.__finaly()
+
+    def __finaly(self):
+        for instance in self.__processors:
+            instance.finalize()
 
     def __should_alarm(self):
         for instance in self.__processors:
-            print instance
             # maybe has the exception in the last 10-20 times?
             has_exception = instance.has_exception()
             # print dict[instance]
             self.__dict[instance.__hash__()].append(has_exception)
-            print instance
-
             if len(self.__dict[instance.__hash__()]) >= self.__num_exceptions_before_alarm:
                 return True
         return False
