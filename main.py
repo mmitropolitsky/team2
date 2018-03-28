832eab879a2ce034c03db737ef23523ed77ab65f
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2018-03-28 17:53:18 +0300

    add OBD data adapter - reaads from the ODB client and returns usable data add base structure for the rest of the behaviour monitoring/analyzing

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from processor.EventProcessor import *
+from adapter.OdbAdapter import *
 
 
 def main():
-    processor = EventProcessor()
-    processor.process()
+    #processor = EventProcessor()
+    #processor.process()
+
+
+    adapter = OdbAdapter()
+    print adapter.speed
 
 
 main()
