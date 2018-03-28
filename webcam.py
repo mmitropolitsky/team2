0ce3c2244cd26ed52af798477ff82e810fd1dff7
milko-taulia <milko.mitropolitsky@taulia.com>
2018-03-28 13:25:01 +0300

    added camera face tracker

---
+++ webcam.py
@@ -0,0 +1,45 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)  # WIDTH
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)  # HEIGHT
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+# upper_body_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # upper_bodies = upper_body_cascade.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30),
+    #     flags=cv2.CASCADE_SCALE_IMAGE)
+    # for (x, y, w, h) in upper_bodies:
+    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    print(len(faces))
+    # Display the resulting frame
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
+        # for (ex, ey, ew, eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
